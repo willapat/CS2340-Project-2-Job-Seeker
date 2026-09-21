5f70b7b7a639216a138c1f9702f94f6f54902377
Ryon Das <ryon.d.das@gmail.com>
2026-09-21 16:33:57 -0400

    Add admin panel: view/delete users and job postings (Story 21)

--- jobseeker_project/settings.py
+++ jobseeker_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'job_seeker',
     'accounts',
+    'admin',
     'jobs',
 ]
 
--- jobseeker_project/urls.py
+++ jobseeker_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin-panel/', include('admin.urls')),
     path('', include('job_seeker.urls')),
     path('accounts/', include('accounts.urls')),
     path('jobs/', include('jobs.urls')),
